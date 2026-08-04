8adedfef2337311c213b755a8c5841acbe7abdda
SMarco2310 <marc.sossou@ashesi.edu.gh>
2026-08-04 15:41:37 +0000

    chore: disable authentication middleware in auth.ts

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,9 +1,9 @@
 export default defineNuxtRouteMiddleware(async () => {
-  const { isSignedIn, fetchCurrentUser, user } = useAuth()
-  if (!isSignedIn.value) {
-    return navigateTo('/login')
-  }
-  if (!user.value) {
-    await fetchCurrentUser()
-  }
+  // const { isSignedIn, fetchCurrentUser, user } = useAuth()
+  // if (!isSignedIn.value) {
+  //   return navigateTo('/login')
+  // }
+  // if (!user.value) {
+  //   await fetchCurrentUser()
+  // }
 })
